7def393014a37bf6f1758260ea5d2f52bdfe7e79
ShreyansT <47342239+ShreyansT@users.noreply.github.com>
2021-03-21 14:01:59 -0500

    Updated tests for update_spec.rb

--- spec/event_feature/update_spec.rb
+++ spec/event_feature/update_spec.rb
@@ -5,17 +5,25 @@ require 'rails_helper'
 # Updating an event
 RSpec.describe 'Updating a event', type: :feature do
   scenario 'valid inputs' do
-    event = Event.create!(name: 'Party')
+    event = Event.create!(name: 'Test Event', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10')
     visit edit_event_path(id: event.id)
     fill_in 'event[name]', with: 'New Party'
+    fill_in 'event[description]', with: 'Fun'
+    fill_in 'event[location]', with: 'College Station'
+    fill_in 'event[time]', with: '6:30 PM'
+    fill_in 'event[points]', with: '5'
     click_on 'Update Event'
     visit events_path
     expect(page).to have_content('New Party')
   end
   scenario 'valid inputs' do
-    event = Event.create!(name: 'Party')
+    event = Event.create!(name: 'Party', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10')
     visit edit_event_path(id: event.id)
     fill_in 'event[name]', with: ''
+    fill_in 'event[description]', with: 'Test'
+    fill_in 'event[location]', with: 'College Station'
+    fill_in 'event[time]', with: '9:30 AM'
+    fill_in 'event[points]', with: '20'
     click_on 'Update Event'
     visit events_path
     expect(page).to have_content('')
