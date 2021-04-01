97e36ac06740dc5cfd0fb05303ffef6bbc3f9bec
ShreyansT <shreyanst@tamu.edu>
2021-03-31 22:28:00 -0500

    fixed testing for events

--- spec/event_feature/update_spec.rb
+++ spec/event_feature/update_spec.rb
@@ -5,31 +5,31 @@ require 'rails_helper'
 # Updating an event
 RSpec.describe 'Updating a event', type: :feature do
   scenario 'valid inputs' do
-    event = Event.create!(name: 'Test Event', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10')
+    event = Event.create!(name: 'Test Event', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10', date: '13 March 2000')
     visit edit_event_path(id: event.id)
     fill_in 'event[name]', with: 'New Party'
     fill_in 'event[description]', with: 'Fun'
     fill_in 'event[location]', with: 'College Station'
     fill_in 'event[time]', with: '6:30 PM'
     fill_in 'event[points]', with: '5'
-    select '1998', from: 'book_date_1i'
-    select 'April', from: 'book_date_2i'
-    select '5', from: 'book_date_3i'
+    select '1998', from: 'event_date_1i'
+    select 'April', from: 'event_date_2i'
+    select '5', from: 'event_date_3i'
     click_on 'Update Event'
     visit events_path
     expect(page).to have_content('New Party')
   end
   scenario 'valid inputs' do
-    event = Event.create!(name: 'Party', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10')
+    event = Event.create!(name: 'Party', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10', date: '15 May 1997')
     visit edit_event_path(id: event.id)
     fill_in 'event[name]', with: ''
     fill_in 'event[description]', with: 'Test'
     fill_in 'event[location]', with: 'College Station'
     fill_in 'event[time]', with: '9:30 AM'
     fill_in 'event[points]', with: '20'
-    select '1999', from: 'book_date_1i'
-    select 'June', from: 'book_date_2i'
-    select '10', from: 'book_date_3i'
+    select '1999', from: 'event_date_1i'
+    select 'June', from: 'event_date_2i'
+    select '10', from: 'event_date_3i'
     click_on 'Update Event'
     visit events_path
     expect(page).to have_content('')
