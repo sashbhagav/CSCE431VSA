1ab4ff8d9d24164b9e20d352663f818f28fabbd7
Sashreek Bhagavatula <sashreek_bhagav@hotmail.com>
2021-04-10 00:30:59 -0500

    Update delete_spec.rb

--- spec/event_feature/delete_spec.rb
+++ spec/event_feature/delete_spec.rb
@@ -5,22 +5,23 @@ RSpec.describe 'Deleting an event', type: :feature do
 
 
         visit 'login/index'
-        sleep (5)
+#         sleep (5)
         within('form') do
           fill_in 'LoginAttempt_username', with: 'cat'
           fill_in 'LoginAttempt_password', with: 'dog'
         end
 
         click_on 'Submit'
-        sleep (5)
+#         sleep (5)
         expect(page).to have_content('VSA Events')
 
 
         event = Event.create!(name: 'Test Event', description: 'Test description', location: 'Houston', time: '3:30 PM', points: '10', date: '20 June 1996', link: 'http://tamu.zoom.us/')
         visit events_path
+        page.driver.browser.manage.window.maximize
         expect(page).to have_content('Test Event')
         visit delete_event_path(id: event.id)
-        sleep (5)
+#         sleep (5)
         click_on 'Delete event'
         expect(page).not_to have_content('Test Event')
         
